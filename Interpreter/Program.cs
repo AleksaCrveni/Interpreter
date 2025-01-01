4b5019a49f0883bcdde11edeae58cc726f6e9f6e
WickedyWick <crvenialeksa@gmail.com>
2025-01-01 17:53:41 +0100

    Inputs handled

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿/******* INPUT *******/
+string filename = "basic.txt";
+if (args.Length == 1)
+{
+  filename = args[0];
+}
+// Change to stream buffer later
+var deep = Directory.GetParent(filename);
+
+string root = deep.Parent.Parent.Parent.Parent.FullName;
+string filepath = Path.Combine(root, "Inputs", filename);
+string input = File.ReadAllText(filepath);
+
+
+
+
+
+
+
+
+
+
