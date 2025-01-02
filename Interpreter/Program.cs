6f7951944fd2f2439e6c218b5b24b329f45e5dd9
WickedyWick <crvenialeksa@gmail.com>
2025-01-02 01:03:26 +0100

    Added test case, need to expand lexer

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -1,15 +1,4 @@
-﻿/******* INPUT *******/
-string filename = "basic.txt";
-if (args.Length == 1)
-{
-  filename = args[0];
-}
-// Change to stream buffer later
-var deep = Directory.GetParent(filename);
-
-string root = deep.Parent.Parent.Parent.Parent.FullName;
-string filepath = Path.Combine(root, "Inputs", filename);
-string input = File.ReadAllText(filepath);
+﻿int i = 0;
 
 
 
---
+++ Interpreter/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+  public static class Utils
+  {
+    public static string LoadInputFromDisk(string filename)
+    {
+      // Change to stream buffer later
+      var deep = Directory.GetParent(filename);
+
+      string root = deep.Parent.Parent.Parent.Parent.FullName;
+      string filepath = Path.Combine(root, "Inputs", filename);
+      return File.ReadAllText(filepath);
+    }
+
+  }
+}
