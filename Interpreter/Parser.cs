890f2bd3d4d52979c5c055fa3c05b0deec49d3d3
WickedyWick <crvenialeksa@gmail.com>
2025-12-24 15:07:07 +0100

    Impelemnt more of parser and add parser tests

--- Interpreter/Parser.cs
+++ Interpreter/Parser.cs
@@ -1,12 +1,144 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Interpreter
+﻿namespace Interpreter
 {
-  internal class Parser
+  public ref struct Parser
   {
+    public Lexer _l;
+    public Token _currToken;
+    public Token _peekToken;
+    public List<string> _errors;
+    public Parser(Lexer l)
+    {
+      _l = l;
+      _currToken = new Token();
+      _peekToken = new Token();
+      _errors = new List<string>();
+      // read two tokens so current and next token are both set
+      NextToken();
+      NextToken();
+    }
+
+    public void NextToken()
+    {
+      _currToken = _peekToken;
+      _peekToken = _l.NextToken();
+    }
+
+    public Program ParseProgram()
+    {
+      Program p = new Program(new List<Statement>());
+      Statement s;
+      while (_currToken.Type != TokenType.EOF)
+      {
+        s = ParseStatement();
+        if (s != null)
+          p._statements.Add(s);
+        NextToken();
+      }
+      return p;
+    }
+
+    public Statement? ParseStatement() =>
+      _currToken.Type switch
+      {
+        TokenType.LET => ParseLetStatement(),
+        _ => null
+      };
+    
+    public Statement? ParseLetStatement()
+    {
+      LetStatement s = new LetStatement();
+      s.token = _currToken;
+
+      if (!ExpectPeek(TokenType.IDENT))
+          return null;
+      s.name = ParseIdentifier();
+
+      if (!ExpectPeek(TokenType.ASSIGN))
+        return null;
+      // TODO: implement parse expression.
+      // for now we will just skip until semicolon;
+      //s.value = ParseExpession();
+
+      while (_currToken.Type != TokenType.SEMICOLON)
+        NextToken();
+      return s;
+    }
+    public Identifier ParseIdentifier()
+    {
+      Identifier i = new Identifier(_currToken, _currToken.Literal);
+      return i;
+    }
+    /*********** PSEUDO **********************/
+    //public Statement ParseLetStatement()
+    //{
+    //  _l.AdvanceToken();
+    //  Identifier ident = ParseIdentifier();
+    //  _l.AdvanceToken();
+
+    //  if (_currToken.Type != TokenType.EQ)
+    //    ParseError("Expecting equal sign!");
+
+    //  Expression exp = ParseExpression();
+    //}
+
+    //public Identifier ParseIdentifier()
+    //{
+    //  Identifier i = new Identifier();
+    //  i.token = _currToken;
+    //  return i;
+    //}
+
+    //public Expression ParseExpression()
+    //{
+    //  if (_currToken.Type == TokenType.INT)
+    //  {
+    //    if (_peekToken.Type == TokenType.PLUS)
+    //      return ParseOperatorExpression();
+    //    else if (_peekToken.Type == TokenType.SEMICOLON)
+    //      return ParseIntegerLiteral();
+    //    else
+    //      ParseError("Unexpected token in expression. Expecting + or ;");
+    //  } else if (_currToken.Type == TokenType.LPAREN)
+    //  {
+    //    return ParseGroupedExpression();
+    //  } else
+    //  {
+    //    ParseError("Unexpected token in expression, expected int or )");
+    //  }
+    //  return null; // do this for comp, i think i will go with idea that parse error should throw expection
+    //}
+
+    //public OperatorExpression ParseOperatorExpression()
+    //{
+    //  OperatorExpression opE = new OperatorExpression();
+
+    //  opE.left = ParseIntegerLiteral();
+    //  _l.AdvanceToken();
+    //  opE.operator = _currToken;
+    //  _l.AdvanceToken();
+    //  opE.right = ParseExpression();
+    //  return opE;
+    //}
+
+    //public void ParseError(string err)
+    //{
+
+    //}
+    public bool ExpectPeek(TokenType t)
+    {
+      if (_peekToken.Type == t)
+      {
+        NextToken();
+        return true;
+      }
+      else
+      {
+        LogPeekError(t);
+        return false;
+      }
+    }
+
+    public void LogPeekError(TokenType t) => _errors.Add($"Expected next token type to be {t}, got {_peekToken.Type}");
+
   }
 }
---
+++ Tests/ParserTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+
+namespace Tests
+{
+  [TestClass]
+  public class ParserTests
+  {
+    [TestMethod]
+    public void TestLetStatementsSuccess()
+    {
+      string input = "let x = 5; let y = 10; let foobar = 838383;";
+      Lexer l = new Lexer(input);
+      Parser p = new Parser(l);
+
+      Program program = p.ParseProgram();
+      CheckParserErrors(p._errors);
+      if (p._errors.Count > 0)
+        Assert.Fail();
+      Assert.IsNotNull(program, "Parse program returned null!");
+      Assert.AreEqual(3, program._statements.Count, $"Expected 3 statements, got {program._statements.Count}");
+
+      string[] tests = ["x", "y", "foobar"];
+      for (int i = 0; i < tests.Length; i++)
+        TestLetStatement(program._statements[i], tests[i]);
+
+    }
+
+    [TestMethod]
+    public void TestLetStatementsFail()
+    {
+      string input = "let x 5; let = 10; let 838383;";
+      Lexer l = new Lexer(input);
+      Parser p = new Parser(l);
+
+      Program program = p.ParseProgram();
+      CheckParserErrors(p._errors);
+      Assert.IsTrue(p._errors.Count > 0);
+    }
+
+
+    public void TestLetStatement(Statement s, string name)
+    {
+      Assert.AreEqual("let", s.TokenLiteral(), $"Statement tokenLiteral is not 'let', got {s.TokenLiteral()}");
+
+      Assert.IsInstanceOfType(s, typeof(LetStatement) ,"Statement is not Let Statement");
+      LetStatement letStatement = (LetStatement)s;
+      Assert.AreEqual(name, letStatement.name.value, $"letStatement.name.value not {name}, got {letStatement.name.value}");
+      Assert.AreEqual(name, letStatement.name.TokenLiteral(), $"letStatement.name.TokenLiteral() not {name}, got {letStatement.name.TokenLiteral()}");
+    }
+
+    public void CheckParserErrors(List<string> errors)
+    {
+      if (errors.Count == 0)
+        return;
+
+      Trace.WriteLine($"Parser has {errors.Count} errors");
+      foreach (string err in errors)
+        Trace.WriteLine(err);
+      
+    }
+  }
+}
